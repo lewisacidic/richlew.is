cb4a7bbe237a0f59c89c5783cc8b1ca93652ad6f
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-11 19:42:45 +0100

    tooling: configure coverage and test reporting
    Adds coverage and test reports, which output in `reports/`

--- jest.config.js
+++ jest.config.js
@@ -13,5 +13,25 @@ module.exports = {
     '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/file-mock.js',
     'typeface-*': '<rootDir>/__mocks__/file-mock.js'
-  }
+  },
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        outputPath: 'reports/html/unit-tests.html',
+        theme: 'lightTheme'
+      }
+    ],
+    [
+      'jest-junit',
+      {
+        output: 'reports/junit/unit-tests.xml'
+      }
+    ]
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'reports/coverage'
 }
