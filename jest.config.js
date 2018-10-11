094c627c92a9c99f90f180359afa649bae1e7bae
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-11 19:12:46 +0100

    tooling: gatsby specific mocks
    Adds a gatsby-shim to mock the gatsby globals. Also add a mock for
    various static file types, and specifically mock `typeface-` packages.

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,12 @@ module.exports = {
   testRegex: '^.+/src/.+\\.spec\\.js$',
   testPathIgnorePatterns: ['node_modules', '.cache'],
   moduleFileExtensions: ['js'],
-  testURL: 'http://localhost'
+  testURL: 'http://localhost',
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  setupFiles: ['<rootDir>/gatsby-shim.js'],
+  moduleNameMapper: {
+    '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+    'typeface-*': '<rootDir>/__mocks__/file-mock.js'
+  }
 }
