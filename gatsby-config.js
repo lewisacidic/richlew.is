7d810c2a39d15f3af217bc9651e0083269a4b1b1
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-11 17:53:17 +0100

    feat: add site metadata to head with react-helmet
    Configures gatsby build step to add site metadata to the head.
    
    - adds react-helmet and the gatsby plugin
    - creates gatsby-config, adds site metadata and registers
      react-helmet plugin.
    - adds `Helmet` component to `Layout`.
    - adds a StaticQuery to add the build data.

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Rich Lewis',
+    author: 'Rich Lewis',
+    siteUrl: 'https://richlew.is',
+    description:
+      'A PhD student at Cambridge University developing Deep Learning applications in Chemistry',
+    keywords: [
+      'rich lewis',
+      'rich',
+      'lewis',
+      'chemistry',
+      'cheminformatics',
+      'chemoinformatics',
+      'molecular informatics',
+      'machine learning',
+      'deep learning',
+      'artificial intelligence',
+      'data science',
+      'computer science',
+      'software engineering',
+      'ml',
+      'ai',
+      'ds',
+      'cs',
+      'phd',
+      'cambridge',
+      'london',
+      'united kingdom',
+      'uk'
+    ].join()
+  },
+  plugins: ['gatsby-plugin-react-helmet']
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 
 import Background from './background'
 import Nav from './nav'
 import Page from './page'
+import { StaticQuery, graphql } from 'gatsby'
 
-const Layout = ({ children }) => (
+const Layout = ({ head, children }) => (
   <Background>
+    <Helmet
+      title={head.title}
+      meta={[
+        { name: 'description', content: head.description },
+        { name: 'keywords', content: head.keywords }
+      ]}
+    />
     <Nav
       items={[
         { path: '/', label: 'home' },
@@ -18,7 +27,35 @@ const Layout = ({ children }) => (
 )
 
 Layout.propTypes = {
+  head: PropTypes.shape({
+    title: PropTypes.string,
+    meta: PropTypes.arrayOf(
+      PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string,
+          content: PropTypes.string
+        })
+      )
+    )
+  }),
   children: PropTypes.node
 }
 
-export default Layout
+const StaticLayout = props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            keywords
+          }
+        }
+      }
+    `}
+    render={data => <Layout head={data.site.siteMetadata} {...props} />}
+  />
+)
+
+export default StaticLayout
