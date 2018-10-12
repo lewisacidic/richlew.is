ee7064473ae066c3f2d2f7ae45791f7977668849
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-12 14:04:51 +0100

    ci: add linting to ci
    Adds a linting job to ci using jest.

--- jest.lint.config.js
+++ jest.lint.config.js
@@ -1,5 +1,21 @@
 module.exports = {
   displayName: 'lint',
   runner: 'jest-runner-eslint',
-  testMatch: ['<rootDir>/src/**/*.js']
+  testMatch: ['<rootDir>/src/**/*.js'],
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        outputPath: 'reports/html/e2e-tests.html',
+        theme: 'lightTheme'
+      }
+    ],
+    [
+      'jest-junit',
+      {
+        output: 'reports/junit/e2e-tests.xml'
+      }
+    ]
+  ]
 }
