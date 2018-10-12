7f38cd046d68a25f84c043e897491ac5aba13b30
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-12 15:37:52 +0100

    tooling(storybook): adds withTheme decorator
    Adds a decorator to add the theme to storybooks.  This is included
    globally for all stories.

--- storybook/decorators.js
+++ storybook/decorators.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { TypographyStyle } from 'react-typography'
+import { ThemeProvider } from 'styled-components'
 
 import typography from '../typography.config'
+import theme from '../src/theme'
 
 export const withTypography = story => (
   <>
     <TypographyStyle typography={typography} />
     {story()}
   </>
+)
+
+export const withTheme = story => (
+  <ThemeProvider theme={theme}>{story()}</ThemeProvider>
 )
