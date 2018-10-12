a6732d4e31c6410ca842185056a77cde480c4b1e
Rich Lewis <richlewis42@users.noreply.github.com>
2018-10-12 13:10:38 +0100

    ci: add a build and e2e job
    Adds a build job which runs yarn build, and e2e job which runs the
    integration tests against the build.

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,5 +2,21 @@ module.exports = {
   displayName: 'e2e',
   preset: 'jest-puppeteer',
   testMatch: ['<rootDir>/src/e2e/**/*.js'],
-  setupTestFrameworkScriptFile: '<rootDir>/jest.e2e.setup.js'
+  setupTestFrameworkScriptFile: '<rootDir>/jest.e2e.setup.js',
+  reporters: [
+    'default',
+    [
+      'jest-html-reporter',
+      {
+        outputPath: 'reports/html/e2e-tests.html',
+        theme: 'lightTheme'
+      }
+    ],
+    [
+      'jest-junit',
+      {
+        output: 'reports/junit/e2e-tests.xml'
+      }
+    ]
+  ]
 }
